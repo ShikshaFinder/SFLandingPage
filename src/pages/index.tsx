e2b4a8584bd2e2f558ea686826410c646b95707a
ShikshaFinder <128922718+ShikshaFinder@users.noreply.github.com>
2023-09-11 21:59:03 +0530

    Update index.tsx
    msjsjs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0"
+          content="width=device-width, initial-scale=0.5"
         ></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
