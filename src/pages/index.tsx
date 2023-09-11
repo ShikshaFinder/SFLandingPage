e89a7d14d7e9e9705cee8f1fea0211d98962daf7
ShikshaFinder <janiharsh794@gmail.com>
2023-09-11 21:50:05 +0530

    rr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,33 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 // import styles from '@/styles/Home.module.css'\
-import Navbar from './components/navbar'
-import Hero from './components/hero'
-import Waitlist from './components/Waitlist'
-import Aboutus from './components/aboutus'
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Waitlist from "./components/Waitlist";
+import Aboutus from "./components/aboutus";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>ShikshaFinder</title>
-        <meta name="description" content="Choose Right to make your future bright" />
-        <meta name="viewport" content="The smartest way to choose a learning platform" />
+        <meta
+          name="description"
+          content="Choose Right to make your future bright"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <Navbar/>
-    <Hero/>
-    <Waitlist/>
-    <Aboutus/>
+      <Navbar />
+      <Hero />
+      <Waitlist />
+      <Aboutus />
     </>
-  )
+  );
 }
