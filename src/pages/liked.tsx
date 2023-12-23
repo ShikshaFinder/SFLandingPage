65b5067f667178728f17ae86cc6628268578bae7
ShikshaFinder <janiharsh794@gmail.com>
2023-12-24 00:47:16 +0530

    blog

---
+++ src/pages/liked.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function liked() {
+  return (
+    <div>liked</div>
+  )
+}
+
+export default liked
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,16 +1,83 @@
 import { Container } from "@chakra-ui/react";
 import React from 'react'
-import { Avatar, WrapItem, Wrap } from "@chakra-ui/react";
-
+import {
+  Avatar,
+  WrapItem,
+  Wrap,
+  Card,
+  CardBody,
+  Badge,
+} from "@chakra-ui/react";
+import { FaAlignLeft, FaCoins, FaEdit, FaHeart, FaLanguage, FaMailBulk, FaMailchimp, FaMapMarkerAlt, FaSchool } from "react-icons/fa";
+import Link from "next/link";
 function profile() {
-  return (<Container>
-    <Wrap>
-    <WrapItem>
-    <Avatar size='2xl' name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />{' '}
-  </WrapItem>
-</Wrap>
+  return (
+    <Container justifyContent={"center"}>
+      <Card bg="gray.100">
+        <CardBody>
+          <Wrap justifyContent={"center"} spacing={4} direction="column">
+            <WrapItem>
+              <Link href={"/formstudent"}>
+                {" "}
+                <Avatar size="2xl" name="harsh" borderRadius={"3xl"} />{" "}
+                <FaEdit size={24} style={{ alignSelf: "flex-end" }} />
+              </Link>
+            </WrapItem>
+            <WrapItem>
+              <big style={{ textAlign: "center" }}>Harsh Jani</big>
+            </WrapItem>
+            <WrapItem>
+              <Badge colorScheme="green" borderRadius={"2xl"}>
+                Good Learner
+              </Badge>
+            </WrapItem>
+            <WrapItem style={{ marginTop: "10px" }}>
+              <FaMapMarkerAlt />
+              <b style={{ textAlign: "center" }}>&nbsp; Ahmedabad, Gujarat</b>
+            </WrapItem>
+            <WrapItem style={{ marginTop: "10px" }}>
+              <FaMailBulk />
+              <b style={{ textAlign: "center" }}>
+                &nbsp;janiharsh794@gmail.com
+              </b>
+            </WrapItem>
+            <WrapItem style={{ marginTop: "10px" }}>
+              <FaCoins />
+              <b style={{ textAlign: "center" }}> 30 </b>
+            </WrapItem>
+            <WrapItem style={{ marginTop: "10px" }}>
+              <FaHeart />
+              <b
+                color="blue"
+                style={{
+                  textAlign: "center",
+                  color: "blue",
+                  textDecoration: "underline",
+                }}
+              >
+                <Link href={"/liked"}> &nbsp; Your Liked Institute</Link>
+              </b>
+            </WrapItem>
+            <WrapItem style={{ marginTop: "10px" }}>
+              <FaAlignLeft />
 
-  </Container>)
+              <b style={{ textAlign: "center" }}>&nbsp; Board</b>
+            </WrapItem>
+            <WrapItem style={{ marginTop: "10px" }}>
+              <FaLanguage />
+
+              <b style={{ textAlign: "center" }}>&nbsp; Medium</b>
+            </WrapItem>
+            <WrapItem style={{ marginTop: "10px" }}>
+              <FaSchool />
+              <b style={{ textAlign: "center" }}>&nbsp; Standard</b>{" "}
+            </WrapItem>
+          </Wrap>
+        </CardBody>
+        {/* Add bg="gray.200" to set the background color */}
+      </Card>
+    </Container>
+  );
 }
 
 export default profile
