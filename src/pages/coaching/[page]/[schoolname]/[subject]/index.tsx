dd24eb06efc4b2870c83868e04940b5ad4833c81
ShikshaFinder <janiharsh794@gmail.com>
2024-01-16 23:53:30 +0530

    not added admissionformlink added

--- src/pages/coaching/[page]/[schoolname]/[subject]/index.tsx
+++ src/pages/coaching/[page]/[schoolname]/[subject]/index.tsx
@@ -12,60 +12,26 @@ import {
 } from "@chakra-ui/react";
 import Cardd from "../../../../../components/card";
 import Admissionform from "../../../../../components/admissionformlink";
+import Videoo from "../../../../../components/video";
+import Subject from "../../../../../components/subject";  
+import InfoTeacher from "../../../../../components/InfoTeacher";
 import Link from "next/link";
 import React from "react";
 
 function IntroSchool() {
   return (
     <>
-      <Stack spacing={4} direction="row" align="center">
-        <Button colorScheme="teal" size="xs">
-          maths
-        </Button>
-        <Button colorScheme="teal" size="xs">
-          science
-        </Button>
-        <Button colorScheme="teal" size="xs">
-          english
-        </Button>
-        <Button colorScheme="teal" size="xs">
-          social science
-        </Button>
-      </Stack>{" "}
       <br />
-      <AspectRatio maxW="560px" ratio={1.75}>
-        <iframe
-          title="Shiksha Finder Intro"
-          src="https://www.youtube.com/embed/pGeHsxjQJXw?si=FLPTuZo-YCnKVMOz"
-          allowFullScreen
-        />
-      </AspectRatio>
-      <Card>
-        <CardHeader>
-          <Heading size="md">Teacher Name</Heading>
-        </CardHeader>
-        <CardBody>
-          <Stack divider={<StackDivider />} spacing="3">
-            <Box>
-              <Heading size="xs" textTransform="uppercase">
-                Experience & Expertice
-              </Heading>
-              <Text pt="2" fontSize="sm">
-                12 to 13 years ....
-              </Text>
-            </Box>
-            <Box>
-              <Heading size="xs" textTransform="uppercase">
-                more about teacher or extracurricular activities
-              </Heading>
-              <Text pt="2" fontSize="sm">
-                ye school bhot badhiya hain mujhe ye school bhot pasand hain
-                sdvfabbjf amnd fadnmd jdbfiaefihfbjd d sfvskbibhdiwbrfhbdb
-              </Text>
-            </Box>
-          </Stack>
-        </CardBody>
-      </Card>
+      <Videoo src="https://www.youtube.com/embed/pGeHsxjQJXw?si=vqQYrO90D7FzrvqN" />
+      <br />
+      <Subject subject1="10" subject2="11" subject3="12" subject4="8" />
+<br />
+      <InfoTeacher
+        TeacherName="Chintansir"
+        Experience={"12 years"}
+        AboutTeacher={"He is a good teacher"}
+        discription={"He is a good teacher"}  />
+
       <Stack direction="row">
         <Link href={"../school/schoolname"}>
           <Cardd
