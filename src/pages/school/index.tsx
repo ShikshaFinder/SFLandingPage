0cb77bcb9b37e8b949ce676f946cb73087560e8f
ShikshaFinder <janiharsh794@gmail.com>
2024-04-07 03:13:57 +0530

    upload image get image succesfull

--- src/pages/school/index.tsx
+++ src/pages/school/index.tsx
@@ -4,7 +4,7 @@ import Bannerad from "../../components/bannerad";
 import Layoutt from "../Layout";
 import supabase from "../../../supabase";
 import { useAuthContext } from "@/context";
-// import { useRouter } from "next/router";
+import { Grid } from "@chakra-ui/react";
 import { useUser } from "@/store";
 
 export default function skillclass() {
@@ -49,25 +49,35 @@ export default function skillclass() {
     <>
       <Layoutt>
         <Bannerad />
-
-        {userData &&
-          userData.map(
-            (
-              school: { schoolname: string; rating: number; link: string,img:string },
-              index: number
-            ) => (
-              <Card
-                key={index} // Ensure unique key for each Card
-                name={school.schoolname}
-                rating={school.rating}
-                link={`/school/${school.schoolname}`}
-                imgsrc={
-                  school.img ||
-                  "https://images.unsplash.com/photo-1612835240301-5b5b8f7e1e8e"
-                }
-              />
-            )
-          )}
+        <Grid
+          templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(4, 1fr)" }}
+          gap={1}
+        >
+          {userData &&
+            userData.map(
+              (
+                school: {
+                  schoolname: string;
+                  rating: number;
+                  link: string;
+                  img: string;
+                },
+                index: number
+              ) => (
+                <Card
+                  key={index} // Ensure unique key for each Card
+                  name={school.schoolname}
+                  rating={school.rating}
+                  link={`/school/${school.schoolname}`}
+                  imgsrc={
+                    school.img
+                      ? ` //wsrv.nl/?url=${school.img}`
+                      : "https://images.unsplash.com/photo-1595528573972-a6e4c0d71f1b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  }
+                />
+              )
+            )}
+        </Grid>
       </Layoutt>
     </>
   );
