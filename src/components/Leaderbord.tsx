932fac0505b82679cc91a364f14db06a8d658d59
ShikshaFinder <janiharsh794@gmail.com>
2024-04-08 02:24:29 +0530

    school leader bord created

--- src/components/Leaderbord.tsx
+++ src/components/Leaderbord.tsx
@@ -1,122 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Box, Divider } from "@chakra-ui/react";
 import {
   Card,
   CardBody,
   Text,
   Stack,
   Avatar,
-  Divider,
-  Box,
   AbsoluteCenter,
 } from "@chakra-ui/react";
 
-
-function Leaderbord({name,name1,name2,name3,number}:{name?:string,number:any,name1?:string,name2?:string,name3?:string}) {
+function LeaderbordName({ name,number,position }: { name?: string,number:number,position:number }) {
   return (
     <>
       <Card>
         <CardBody>
-          <Stack direction="row" spacing={"8"}>
-            <Avatar name={name} />
+          <Stack direction="row" spacing={"8"} display={"flex"} alignItems={"center"} justifyContent={"space-between"}>
+            <Box display={"flex"} alignItems={"center"}>
+              <Avatar name={name} />&nbsp;&nbsp;  
+              <Text
+                // display="flex"
+                // alignItems="center"
+                // justifyContent="center"
+                fontWeight="bold"
+              >
+                {name}
+              </Text>
+            </Box>
             <Text
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
+              // display="flex"
+              // alignItems="center"
+              // justifyContent="space-between"
+
               fontWeight="bold"
             >
-              {name}
-            </Text>
-            <Text
-              display="flex"
-              fontSize="xl"
-              alignItems={"center"}
-              fontWeight="bold"
-            >
-              {number}{" "}
-            </Text>
-          </Stack>
-        </CardBody>
-      </Card>
-      <Box position="relative" padding="10">
-        <Divider />
-        <AbsoluteCenter  px="4">
-          Leaderbord
-        </AbsoluteCenter>
-      </Box>
-      <Card>
-        <CardBody>
-          <Stack direction="row" spacing={"8"}>
-            <Avatar name={name1} />
-            <Text
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              fontWeight="bold"
-            >
-              {name1}
-            </Text>
-            <Text
-              display="flex"
-              fontSize="xl"
-              alignItems={"center"}
-              fontWeight="bold"
-              color="yellow.500"
-            >
-              1st
+              {position}
             </Text>
           </Stack>
         </CardBody>
       </Card>
-      <Card>
-        <CardBody>
-          <Stack direction="row" spacing={"8"}>
-            <Avatar name={name2} />
-            <Text
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              fontWeight="bold"
-            >
-              {name2}
-            </Text>
-            <Text
-              display="flex"
-              fontSize="xl"
-              alignItems={"center"}
-              fontWeight="bold"
-              color="silver"
-            >
-              2nd
-            </Text>
-          </Stack>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <Stack direction="row" spacing={"8"}>
-            <Avatar name={name3} />
-            <Text
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              fontWeight="bold"
-            >
-              {name3}
-            </Text>
-            <Text
-              display="flex"
-              fontSize="xl"
-              alignItems={"center"}
-              fontWeight="bold"
-              color="green"
-            >
-             3rd
-            </Text>
-          </Stack>
-        </CardBody>
-      </Card>
+      <br />
     </>
   );
 }
 
-export default Leaderbord
+export default LeaderbordName;
--- src/pages/schoolleaderbord.tsx
+++ src/pages/schoolleaderbord.tsx
@@ -1,20 +1,72 @@
 import React from 'react'
 import Leaderbord from '../components/Leaderbord'
 import Layout from './Layout';
+import { useState } from 'react';
+import supabase from '../../supabase';
+import { useEffect } from 'react';
+import { useAuthContext } from '@/context';
 
-function schoolleaderbord() {
+import {  Text  } from "@chakra-ui/react";
+
+interface LeaderboardEntry {
+  schoolname: string;
+  views: number;
+  name1: string;
+  name2: string;
+  name3: string;
+}
+
+
+function Schoolleaderbord() {
+
+  const { user } = useAuthContext();
+  
+const [leaderboardData, setLeaderboardData] = useState<LeaderboardEntry[]>([]);
+
+  const fetchLeaderboardData = async () => {
+
+
+    const { data, error } = await supabase
+      .from("School")
+      .select("view , schoolname, Board")
+      .order("view", { ascending: false })
+      .limit(10); 
+      // .eq("District", user?.Board)
+
+      // console.log({data,error});
+
+      
+    if (error) {
+      console.error("Error fetching leaderboard data:", error);
+    } else {
+      setLeaderboardData(data as never[]);
+    }
+      }
+
+
+       useEffect(() => {
+         fetchLeaderboardData();
+       }, [user]);
+
   return (
-    <><Layout>
-      <Leaderbord
-        name="your school"
-        number={78}
-        name1="Shree Ram"
-        name2="Harsh Jani"
-        name3="rudra joshi"
-      />
-    </Layout>
+    <>
+      <Layout>
+        <Text fontSize="3xl" display={"flex"} justifyContent={"center"}>Leaderbord</Text>
+<br />
+        {leaderboardData.map((entry, index) => (
+          <Leaderbord
+            key={index}
+            name={entry.schoolname}
+            number={entry.views}
+            position={index + 1}
+          />
+          
+        ))}
+      </Layout>
     </>
   );
 }
 
-export default schoolleaderbord
+export default Schoolleaderbord;
+
+//for students shiksha coin earned hase ema evu jova nu k khali plus valu j thay minus valu na thay
