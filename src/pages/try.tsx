bccd27e6ce300bb8d59c336bb38f47c185742d18
ShikshaFinder <janiharsh794@gmail.com>
2024-04-20 13:25:21 +0530

    xknvl

--- src/pages/try.tsx
+++ src/pages/try.tsx
@@ -1,11 +1,43 @@
 import React from 'react'
 import Standard from '@/components/Standard'
 
+
+declare global {
+  interface Window {
+    BeforeInstallPromptEvent: any;
+  }
+}
 function Try() {
-  return (<>
-  <Standard name='ksjf'/>
-  </>
-  )
+
+  const handleInstallClick = () => {
+   // ...
+console.log("install button clicked");
+   if ("serviceWorker" in navigator) {
+    console.log("service worker is available");
+     window.addEventListener("beforeinstallprompt", (event: any) => {
+        console.log("beforeinstallprompt fired");
+       event.preventDefault();
+       const installPromptEvent = event as any;
+       installPromptEvent.prompt();
+       installPromptEvent.userChoice.then(
+         (choiceResult: { outcome: string }) => {
+           if (choiceResult.outcome === "accepted") {
+             console.log("User accepted the install prompt");
+           } else {
+             console.log("User dismissed the install prompt");
+           }
+         }
+       );
+     });
+   }
+ };
+
+  return (
+    <>
+      {/* <Standard name="ksjf" /> */}
+      <button id="installApp" onClick={handleInstallClick}>Install</button>
+    </>
+  );
 }
 
 export default Try
