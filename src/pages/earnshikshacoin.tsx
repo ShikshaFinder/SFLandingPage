0b064ee104adc7a1842e42b6440b59d10fdac04c
ShikshaFinder <janiharsh794@gmail.com>
2024-04-26 11:48:33 +0530

    share button added

--- src/pages/earnshikshacoin.tsx
+++ src/pages/earnshikshacoin.tsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import { Button, Stack } from '@chakra-ui/react'
-import Link from 'next/link'
+import React from "react";
+import { Button, Stack } from "@chakra-ui/react";
+import Link from "next/link";
 
 function earnshikshacoin() {
-  return (
-    <div>Shiksha Coin refferal program is still in development ,we are sorry for incovinience
+  const handleShare = () => {
+    let slugs = window.location.pathname.split("/");
+    slugs = slugs.filter((slug) => slug !== "");
+    let shareUrl = "https://shikshafinder.com/";
 
-        <Link href={"/"}  ><Button>Go Back to Home</Button></Link>
-    </div>
-  )
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "Shiksha Finder",
+          text: "I found this website named shiksha finder ,it might be help for you too.",
+          url: shareUrl,
+        })
+        .then(() => console.log("Successful share"))
+        .catch((error) => console.log("Error sharing", error));
+    } else {
+      console.log("Web Share API not supported");
+    }
+  };
+ 
+  return (
+    <>
+      <div>
+        Shiksha Coin refferal program is still in development ,we are sorry for
+        incovinience
+      </div>
+      <br /><br />
+      <Stack spacing={14} direction="row" align="center" >
+        <Link href="/">
+          <Button colorScheme="teal">home page</Button>
+        </Link>
+        <Button colorScheme="teal" onClick={handleShare}>
+          Share
+        </Button>
+      </Stack>
+    </>
+  );
 }
 
-export default earnshikshacoin
+export default earnshikshacoin;
