ff3d1fa870313c7f08bba8efe4197cdc9d82cdc8
ShikshaFinder <janiharsh794@gmail.com>
2024-04-28 16:25:02 +0530

    icons corrected

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           title="Shiksha Finder"
           description="Showcase the quality of education"
           openGraph={{
-            url: "https://shikshafinder.com/",
+            url: "https://images.unsplash.com/photo-1532012197267-da84d127e765?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             title: "Let's promote Quality of Education",
             description: "Choose right educational platform for your child",
             images: [
