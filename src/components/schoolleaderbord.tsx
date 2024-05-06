4755cf5d3d7d50f14d4fef1af56c386d38320486
ShikshaFinder <janiharsh794@gmail.com>
2024-05-06 16:14:53 +0530

    leader bord

--- src/components/schoolleaderbord.tsx
+++ src/components/schoolleaderbord.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Leaderbord from "./Leaderbord";
-import { useState } from "react";
-import supabase from "../../supabase";
-import { useEffect } from "react";
-import { useAuthContext } from "@/context";
 
-import { Text } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { InfoIcon } from "@chakra-ui/icons";
 
 interface LeaderboardEntry {
   schoolname: string;
@@ -16,50 +13,59 @@ interface LeaderboardEntry {
 }
 
 function Schoolleaderbord() {
-  const { user } = useAuthContext();
+  //   const { user } = useAuthContext();
 
-  const [leaderboardData, setLeaderboardData] = useState<LeaderboardEntry[]>(
-    []
-  );
-/////change
-  const fetchLeaderboardData = async () => {
-    const { data, error } = await supabase
-      .from("School")
-      .select("view , schoolname, Board")
-      .order("view", { ascending: false }) //here i can sort with help of vote also
-      .limit(10);
-    // .eq("District", user?.Board)
-    //percentage * rating
+  //   const [leaderboardData, setLeaderboardData] = useState<LeaderboardEntry[]>(
+  //     []
+  //   );
+  // /////change///
+  //   const fetchLeaderboardData = async () => {
+  //     const { data, error } = await supabase
+  //       .from("votes")
+  //       .select("view , rating, user_id")
+  //       .order("view", { ascending: false }) //here i can sort with help of vote also
+  //       .limit(10);
+  //     // .eq("District", user?.Board)
+  //     //percentage * rating
 
-    if (error) {
-      console.error("Error fetching leaderboard data:", error);
-    } else {
-      setLeaderboardData(data as never[]);
-    }
-  };
+  //     if (error) {
+  //       console.error("Error fetching leaderboard data:", error);
+  //     } else {
+  //       setLeaderboardData(data as never[]);
+  //     }
+  //   };
 
-  useEffect(() => {
-    fetchLeaderboardData();
-  }, [user]);
+  //   useEffect(() => {
+  //     fetchLeaderboardData();
+  //   }, [user]);
 
   return (
     <>
       <Text fontSize="3xl" display={"flex"} justifyContent={"center"}>
         Leaderbord
       </Text>
-      <br />
-      {leaderboardData.map((entry, index) => (
+      <br />{" "}
+      <Box textAlign="center" py={10} px={6}>
+        <InfoIcon boxSize={"50px"} color={"blue.500"} />
+        <Heading as="h2" size="xl" mt={6} mb={2}>
+          Leaderboard will be available soon. Please visit again after some time
+        </Heading>
+        <Text color={"gray.800"}>
+          We are excited to showcase which students and educational institutes
+          are performing great in your district. Once we receive sufficient
+          applicants, we will showcase them here!
+        </Text>
+      </Box>
+      {/* {/* //   {leaderboardData.map((entry, index) => (
         <Leaderbord
           key={index}
           name={entry.schoolname}
           number={entry.views}
           position={index + 1}
-        />
-      ))}
+        /> */}
     </>
   );
 }
 
 export default Schoolleaderbord;
 
-//for students shiksha coin earned hase ema evu jova nu k khali plus valu j thay minus valu na thay
