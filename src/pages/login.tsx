07d92c252e3307e4e3fe219b80a07033f74e20ee
ShikshaFinder <janiharsh794@gmail.com>
2024-05-07 19:32:41 +0530

    error detection

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -55,10 +55,21 @@ export default function Login() {
         email,
         password,
       });
+      if(error) {
+        toast({
+          title: "Error.",
+          description: error.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+        return;
+      }
 
       setTimeout(() => {
         router.reload();
       }, 2000);
+      
       router.push("/school");
     } catch (error) {
       console.log(error);
