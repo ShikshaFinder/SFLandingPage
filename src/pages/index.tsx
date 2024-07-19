5797347a7853e6f08a273b89cd90c4dba78801c7
ShikshaFinder <janiharsh794@gmail.com>
2024-07-19 12:47:28 +0530

    k

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home() {
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
-          />
+          />                  
         </Head>
         <Container maxW={"7xl"}>
           <Stack
